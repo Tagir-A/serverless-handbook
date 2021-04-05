a833892079ae43527a24917b25832b31a9eb5c7c
Tagir Almasov <tccup1@gmail.com>
2021-04-05 12:54:43 +0000

    add api

--- src/handler.ts
+++ src/handler.ts
@@ -4,6 +4,6 @@ export const hello = async (event: APIGatewayEvent) => {
   console.log('should go to CloudWatch')
   return {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello there, handsome!', eMethod: event.httpMethod })
+    body: JSON.stringify({ msg: 'Hello there, handsome!!!', eMethod: event.httpMethod })
   }
 }
